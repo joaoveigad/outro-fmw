0c070a03bad158f6b93a5f9a67c2bbff319149b2
João Veiga <jvdvandrade@outlook.pt>
2024-05-31 16:47:52 -0300

    correção erros

--- livros-angular/src/app/controle-livros.service.ts
+++ livros-angular/src/app/controle-livros.service.ts
@@ -1,9 +1,31 @@
+// src/app/controle-livros.service.ts
 import { Injectable } from '@angular/core';
+import { Livro } from './Livro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleLivrosService {
+  private livros: Array<Livro> = [
+    { codigo: 1, codEditora: 1, titulo: 'Livro A', resumo: 'Resumo A', autores: ['Autor A1', 'Autor A2'] },
+    { codigo: 2, codEditora: 2, titulo: 'Livro B', resumo: 'Resumo B', autores: ['Autor B1'] },
+    { codigo: 3, codEditora: 3, titulo: 'Livro C', resumo: 'Resumo C', autores: ['Autor C1', 'Autor C2', 'Autor C3'] }
+  ];
 
-  constructor() { }
+  obterLivros(): Array<Livro> {
+    return this.livros;
+  }
+
+  incluir(livro: Livro): void {
+    const novoCodigo = Math.max(...this.livros.map(l => l.codigo)) + 1;
+    livro.codigo = novoCodigo;
+    this.livros.push(livro);
+  }
+
+  excluir(codigo: number): void {
+    const indice = this.livros.findIndex(l => l.codigo === codigo);
+    if (indice !== -1) {
+      this.livros.splice(indice, 1);
+    }
+  }
 }
--- livros-angular/src/app/livro-lista/livro-lista.component.ts
+++ livros-angular/src/app/livro-lista/livro-lista.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+// src/app/livro-lista/livro-lista.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Editora } from '../Editora';
+import { Livro } from '../Livro';
 
 @Component({
   selector: 'app-livro-lista',
-  standalone: true,
-  imports: [],
   templateUrl: './livro-lista.component.html',
-  styleUrl: './livro-lista.component.css'
+  styleUrls: ['./livro-lista.component.css']
 })
-export class LivroListaComponent {
+export class LivroListaComponent implements OnInit {
+  public editoras: Array<Editora> = [];
+  public livros: Array<Livro> = [];
 
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService
+  ) {}
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+    this.livros = this.servLivros.obterLivros();
+  }
+
+  excluir = (codigo: number): void => {
+    this.servLivros.excluir(codigo);
+    this.livros = this.servLivros.obterLivros();
+  }
+
+  obterNome = (codEditora: number): string => {
+    return this.servEditora.getNomeEditora(codEditora);
+  }
 }
